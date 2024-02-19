089b17ae26e78569a5ae2c5d4e1daf5bd416cc0a
mdmonis <mdmonis128@gmail.com>
2024-02-19 20:36:03 +0530

    Duplicate array and empty array

---
+++ 7_ArrayEmptyDupli.js
@@ -0,0 +1,11 @@
+// function duplicate(arr){
+//     return arr.concat(arr)
+// }
+
+
+// console.log(duplicate([2,5,6,4]))
+
+let arr= [1,2,5,6]
+console.log(arr)
+arr.length = 0;
+console.log(arr)
