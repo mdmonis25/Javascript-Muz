1219ac6848b2e4a86c07bc65cdf59a6e0684765e
mdmonis <mdmonis128@gmail.com>
2024-02-24 11:48:31 +0530

    Missing Number

---
+++ 12_DisplayLikes.js
@@ -0,0 +1,27 @@
+// function DisplayLikes(names){
+//     const len = names.length;
+//     if(len == 0){
+//         return "No one likes this";
+//     }
+//     else if(len == 1){
+//         return `${names[0]} likes this.`;
+//     }
+//     else if(len == 2){
+//         return `${names[0]} and ${names[1]} likes this.`;
+//     }
+//     else if(len == 3){
+//         return `${names[0]}, ${names[1]} and ${names[2]} likes this.`;
+//     }
+//     else if(len >3 ){
+//         return `${names[0]}, ${names[1]} and ${len-2} others likes this.`;
+//     }
+// }
+
+// console.log(DisplayLikes(["monis"]))
+// console.log(DisplayLikes(["monis","kunal"]))
+// console.log(DisplayLikes(["monis","Anurag","Gajendra"]))
+// console.log(DisplayLikes(["monis","kunal","Anurag","Amit","Gajendra"]))
+// console.log(DisplayLikes(["monis","kunal","Anurag","Amit","Gajendra","Tarun","Ajay","Ritik"]))
+
+console.log(`2 + 4 is equal to ${2+4}`)
+console.log("2 + 4 is equal to",2+4)
